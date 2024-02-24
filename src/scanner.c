ce2f4f66454edb69f66bfa3a1884f601fccd9e08
ObserverOfTime <chronobserver@disroot.org>
2024-02-24 10:51:30 +0200

    refactor(scanner): drop unnecessary struct

--- src/scanner.c
+++ src/scanner.c
@@ -1,40 +1,22 @@
 #include "tree_sitter/parser.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 enum TokenType { FAKE_EOL };
 
-typedef struct {
-  bool reached_eof;
-} Scanner;
-
-void *tree_sitter_properties_external_scanner_create() {
-  Scanner *scanner = malloc(sizeof(Scanner));
-  scanner->reached_eof = false;
-  return scanner;
-}
+static bool reached_eof = false;
 
 bool tree_sitter_properties_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
-  Scanner *scanner = (Scanner *) payload;
-
-  if (valid_symbols[FAKE_EOL] && !scanner->reached_eof && lexer->eof(lexer)) {
-    lexer->result_symbol = FAKE_EOL;
-    return scanner->reached_eof = true;
-  }
-
-  return false;
+  lexer->result_symbol = FAKE_EOL;
+  return reached_eof = !reached_eof && valid_symbols[FAKE_EOL] && lexer->eof(lexer);
 }
 
 unsigned tree_sitter_properties_external_scanner_serialize(void *payload, char *buffer) {
-  Scanner *scanner = (Scanner *) payload;
-  return scanner->reached_eof ? 1 : 0;
+  return reached_eof;
 }
 
 void tree_sitter_properties_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
-  Scanner *scanner = (Scanner *) payload;
-  scanner->reached_eof = length == 1;
+  reached_eof = length;
 }
 
-void tree_sitter_properties_external_scanner_destroy(void *payload) {
-  free(payload);
-}
+void *tree_sitter_properties_external_scanner_create() { return NULL; }
+
+void tree_sitter_properties_external_scanner_destroy(void *payload) { }
